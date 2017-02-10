80d8fc433e943b8c051a0c27f57e9e68b1e9babe
Ranjith Zachariah <ranjith.zachariah@gmail.com>
2017-02-10 18:55:13 -0500

    WIP

---
+++ schema/query.js
@@ -0,0 +1,30 @@
+import {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList
+} from 'graphql';
+
+import Trade from './trade';
+
+export default new GraphQLObjectType({
+  name: 'Query',
+  description: 'The root query',
+  fields: {
+    goodbye: {
+      type: GraphQLString,
+      resolve: () => 'hackathon'
+    },
+    trades: {
+      type: new GraphQLList(Trade),
+      resolve: () => {
+          return [
+              {
+                  symbol: "IBM",
+                  action: "BUY",
+                  amount: 100
+              }
+          ];
+      }
+    }
+  }
+})
